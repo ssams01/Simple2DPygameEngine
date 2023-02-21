aa87132085b4d3c738a7bd0f2d59b58ca8654b3f
Stephen Thomas Sams <97904794+ssams01@users.noreply.github.com>
2023-02-21 13:03:12 -0500

    Add files via upload

---
+++ actor/action/drawLetter.py
@@ -0,0 +1,41 @@
+import pygame
+
+class DrawLetter: 
+    def __init__(self):
+        self.types = ["display"]
+        self.entity_state = None
+        self.verbose = False
+        self.name = "draw_letter_action"
+        return
+    
+    def condition_to_act(self,data):
+       if self.entity_state == None:
+          return False
+       if self.entity_state.active == False:
+          return False
+
+       #checks to see if there has been any data passed because, can't draw
+       #if there is no letter 
+       if data == None:
+          return False
+       return True
+
+    def act(self,data):
+       if self.condition_to_act(data):
+          self.draw(data)
+          if self.verbose:
+             print(self.name + " for " + self.entity_state.name)
+       return
+    
+     #draws a letter to the screen with the given specifications it gets from the passed in instance of Letter
+     #(represented by self)
+    def draw(self, screen):
+
+       #specifying the font style (default) and the font size
+       font = pygame.font.Font(None, 50)
+
+       letter = font.render(str(self.entity_state.letter), True, self.entity_state.color)
+       screen.blit(letter, self.entity_state.location)
+       return 
+
+    
